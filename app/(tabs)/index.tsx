0db71d46a60e53e5d678ed020a9f277b821b148d
Adnan <adbak.dev@gmail.com>
2024-12-25 20:30:41 +0100

    image picker

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,28 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet } from "react-native";
 import { theme } from "@/theme";
+import { usePlantStore } from "@/store/plantsStore";
+import { PlantCard } from "@/components/PlantCard";
+import { PlantlyButton } from "@/components/PlantlyButton";
+import { useRouter } from "expo-router";
 
 export default function App() {
+	const plants = usePlantStore((state) => state.plants);
+	const router = useRouter();
+
+	const handleAddPlant = () => {
+		router.navigate("/new");
+	};
+
 	return (
-		<View style={styles.container}>
-			<Text>Open up App.tsx to start working on your app!</Text>
-			<StatusBar style="auto" />
-		</View>
+		<FlatList
+			style={styles.container}
+			contentContainerStyle={styles.contentContainer}
+			data={plants}
+			renderItem={({ item }) => <PlantCard plant={item} />}
+			ListEmptyComponent={() => (
+				<PlantlyButton title="Add a plant" onPress={handleAddPlant} />
+			)}
+		/>
 	);
 }
 
@@ -15,7 +30,9 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: theme.colors.white,
-		alignItems: "center",
-		justifyContent: "center",
+	},
+	contentContainer: {
+		paddingHorizontal: 16,
+		paddingTop: 16,
 	},
 });
